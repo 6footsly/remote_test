014dc7decda8fa227aa363e0d984efb71767f214
6footsly <140808339+6footsly@users.noreply.github.com>
2023-11-09 15:18:01 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "132d44fc-827d-468f-bd72-09409705a1b0"
+  client_id       = "3ebfc7c8-8c9e-4061-80ff-0431492a1aed"
+  client_secret   = "Hqc8Q~eDpzEwa6vBV.go3yOEOS4oS-FavJvLjazx"
+  tenant_id       = "4bcf17d2-d890-4369-a0b5-ceff8e65262d"
+}
+
+
+resource "azurerm_storage_account" "example" {
+  name                     = "mystorage222"
+  resource_group_name      = "strrg"
+  location                 = "East US"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
